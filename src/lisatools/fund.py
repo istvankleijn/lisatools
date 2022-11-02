bdc06ef5ccb404b27c16ccb6484c6a0cd8d9b38c
Istvan Kleijn <istvankleijn@users.noreply.github.com>
2022-11-02 14:34:13 +0000

    Implemented Fund class

--- src/lisatools/fund.py
+++ src/lisatools/fund.py
@@ -2,8 +2,51 @@ import datetime
 from collections import namedtuple
 
 
-Fund = namedtuple(
-    "Fund",
-    ["symbol", "description", "asset_class", "price", "date"],
-    defaults=["DEFAULT", "Default fund", "NA", 1.0, datetime.date.today()]
-    )
+class Fund:
+    """
+    Details of a fund, including its current market price.
+    
+    Attributes
+    ----------
+    description : str
+        Description of the fund, for example the name given by its provider.
+    price : float
+        Latest market price available for the fund.
+    isin : str or None
+        If a real fund, the International Securities Identification Number
+        (ISIN) of the fund, otherwise None.
+        The ISIN is a unique 12-character alphanumerical identifier.
+    date : datetime.date
+        Date at which the `price` was last updated.
+    
+    Examples
+    --------
+
+    Constructing a Fund from a description and current price.
+    
+    >>> f = lisatools.Fund("FTSE Global All Cap Index Fund", 170.14)
+
+    Constructing a Fund with all optional details.
+
+    >>> f = lisatools.Fund("FTSE Global All Cap Index Fund", 170.14,
+    ...                    isin="GB00BD3RZ582",
+    ...                    date=datetime.date(2022, 11, 1))
+    """
+
+    def __init__(self,
+                 description="Default fund",
+                 price=1.0,
+                 *,
+                 isin=None,
+                 date=datetime.date.today()):
+        self.description = description        
+        self.isin = isin       
+        self.update_price(price, date=date)
+    
+    def update_price(self, price, *, date=datetime.date.today()):
+        """Set the price of the fund to the given value. Specify the date when
+        the fund had the given price by the keyword `date`. If left unspecified,
+        set the date to today.
+        """
+        self.price = price
+        self.date = date
--- tests/test_lisatools.py
+++ tests/test_lisatools.py
@@ -1,8 +1,22 @@
-from lisatools import fund, portfolio
+import datetime
+import lisatools
 
 
+def test_fund():
+    """Test the methods of Fund class."""
+    f = lisatools.Fund("FTSE Global All Cap Index Fund", 123.45)
+    assert f.description == "FTSE Global All Cap Index Fund"
+    assert f.price == 123.45
+    assert f.isin is None
+    assert f.date == datetime.date.today()
+    f.update_price(170.14, date=datetime.date(2022, 11, 1))
+    assert f.description == "FTSE Global All Cap Index Fund"
+    assert f.price == 170.14
+    assert f.isin is None
+    assert f.date == datetime.date(2022, 11, 1)
+    
 def test_holding():
-    """Test methods of Holding class"""
-    f = fund.Fund(price = 2.0)
-    h = portfolio.Holding(f, units = 3.0)
+    """Test the methods of the Holding class."""
+    f = lisatools.Fund(price = 2.0)
+    h = lisatools.Holding(f, units = 3.0)
     assert h.value() == 6.0
