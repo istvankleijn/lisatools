76b9834d4792a686970ae36277def882052f9059
Istvan Kleijn <istvankleijn@users.noreply.github.com>
2022-11-08 17:07:42 +0000

    Write tests using pytest fixtures/parametrizations

--- tests/test_lisatools.py
+++ tests/test_lisatools.py
@@ -1,22 +1,40 @@
 import datetime
 import lisatools
+import pytest
 
 
-def test_fund():
-    """Test the methods of Fund class."""
-    f = lisatools.Fund("FTSE Global All Cap Index Fund", 123.45)
+@pytest.fixture
+def ftse_global():
+    d = "FTSE Global All Cap Index Fund"
+    f = lisatools.Fund(d, 123.45)
+    return f
+
+def test_fund_init(ftse_global):
+    """Test the constructor of the `Fund` class"""
+    f = ftse_global
     assert f.description == "FTSE Global All Cap Index Fund"
     assert f.price == 123.45
     assert f.isin is None
     assert f.date == datetime.date.today()
+
+def test_fund_update_price(ftse_global):
+    """Test the `update_price` method of the `Fund` class."""
+    f = ftse_global
     f.update_price(170.14, date=datetime.date(2022, 11, 1))
-    assert f.description == "FTSE Global All Cap Index Fund"
+    assert f.description == ftse_global.description
     assert f.price == 170.14
     assert f.isin is None
     assert f.date == datetime.date(2022, 11, 1)
-    
-def test_holding():
-    """Test the methods of the Holding class."""
-    f = lisatools.Fund(price = 2.0)
-    h = lisatools.Holding(f, units = 3.0)
-    assert h.value() == 6.0
+
+@pytest.mark.parametrize(
+    "p, u, res",
+    [
+        (2.0, 3.0, 6.0),
+        (2.5, 1.0, 2.5)
+    ]
+)
+def test_holding_value(p, u, res):
+    """Test the 'value' method of the Holding class."""
+    f = lisatools.Fund(price = p)
+    h = lisatools.Holding(f, units = u)
+    assert h.value() == pytest.approx(res)
